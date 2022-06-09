41ebb7cdf8bbd164e1d4982016fb5991fe0398ba
tarunmandhan <mandhantarun@gmail.com>
2022-06-09 22:51:44 +0530

    uploading

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom' ;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+<>
+<h1> Hello tarun </h1>
+<p> this is my first project </p>
+<p> this is my 2nd project </p>
+<h2> this is 3rd project </h2>
+</>
+,document.getElementById("root")); 
